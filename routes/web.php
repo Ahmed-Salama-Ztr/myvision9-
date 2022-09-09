add8130f14c22998bdf55f0334d37b42ce676c8e
Ahmed-Salama-Ztr <ahmed.zo.2001@outlook.com>
2022-09-09 04:45:47 +0300

    First website

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Site1Controller;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,10 +14,15 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 
 Route::get('about',function(){
     return 'About us';
 });
+Route::get('contact',function(){
+    return 'Hello contact';
+});
+Route::get('site1/about',[Site1Controller::class,'index'])->name('index');
+
